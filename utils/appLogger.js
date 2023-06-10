82b95dcf29bceed3b3ed9033487656e4eefc1a72
Sayeeduddin <iamsayeeduddin@outlook.com>
2023-06-10 17:40:05 +0530

    index changes

--- utils/appLogger.js
+++ utils/appLogger.js
@@ -2,10 +2,14 @@ const bunyan = require("bunyan");
 const path = require("path");
 
 const dir = path.join(__dirname, "..", "logs");
+const logPath = path.join(dir, "app.log");
+if (!fs.existsSync(logPath)) {
+  fs.writeFile(logPath);
+}
 
 const logger = bunyan.createLogger({
   name: "ECom",
-  streams: [{ path: path.join(dir, "app.log") }],
+  streams: [{ path: logPath }],
 });
 
 module.exports = logger;
